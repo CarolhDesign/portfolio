dbfa0dffc319e431582e93f8199850789a226fab
CarolhDesign <carole.haurat@gmail.com>
2022-03-26 20:28:08 +0100

    Amélioration du code

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ fetch(url)
 .then(response => response.json())
 .then(datas => displayData(datas))
 
+//let ramdom = 0
 
+
+
+document.querySelector('body').addEventListener('click', (e) => {
+           
+    if(e.target.classList.contains('heart')){
+     e.target.classList.toggle('fa-solid')
+     e.target.classList.toggle('fa-regular')
+    
+    }  })
+ 
 
 function displayData(datas){
     const ul = document.querySelector('.folio')
@@ -13,7 +24,6 @@ function displayData(datas){
     for(data in datas){
         // On créer une boucle for in pour parcourir les index du tableau d'objet.
         // Ensuite, on créer les variables pour une meilleure accessibilité et lisibilité du code
-
         let titre = datas[data].title
         let id = datas[data].id
         let image = datas[data].image
@@ -21,18 +31,53 @@ function displayData(datas){
         let isLiked = datas[data].isLiked
         let isNew = datas[data].isNew
         let logiciel = datas[data].logiciel
+        let visible =  'hidden'
+        const displayLiked = isLiked == 'true' ?  'fa-solid' : 'fa-regular'
+
+      //Verifier si new Post
+            if(isNew == "true"){
+                visible = 'visible'
+            }
+
+        // Vérifier Logiciels
+
+        if(logiciel == "AI"){
+            log = 'ai'
+        } else if (logiciel == "PS"){
+            log = 'ps'
+        } else if (logiciel == "In"){
+            log = "in"
+        } else if(logiciel == "Affinity Designer"){
+            log = "afdesigner"
+        } else if (logiciel == "Affinity Publisher"){
+            log = "afpublisher"
+        } else {
+            log = "hidden"
+        }
+       
+        //Vérifier Types
+
+        if(type == "Logo"){
+            tag = 'logo'
+        } else if (type == "Web"){
+            tag = 'web'
+        } else if (type == "Vectoriel"){
+            tag = "vector"
+        } else {
+            log = "hidden"
+        }
 
         ul.innerHTML += `
         <li class="portfolio-item">
 
         <div id="header">
-            <div class="new-item">
-                <div class="blocNew">
-                    <h2>Nouveauté</h2>
-                </div>
+            <div class="new-item ${visible} ">
+                  <div class="blocNew">
+                        <h2>Nouveauté</h2>
+                  </div>
             </div>
-            <div class="like">
-                <div class="icon"><i class="heart fa-regular fa-heart"></i></div>
+            <div class="like ">
+                <div class="icon"><i class="heart ${displayLiked} fa-heart"></i></div>
             </div>
         </div>
         <div class="portfolio-content">
@@ -40,181 +85,23 @@ function displayData(datas){
             <div class="title">${titre}</div>
         </div>
         <div class="portfolio-footer">
-            <div class="tag">${type}</div>
-            <div class="logiciel-items">${logiciel}</div>
+            <div class="tag">
+            <div class="tagFond ${tag}">
+                <p class="tagName">${type}</p>
+            </div>
+            </div>
+            <div class="logiciel-items">
+                <div class="fond ${log}">
+                    <p class="text">${logiciel}</p>
+                </div>
+            </div>
         </div>
     </li>
         `;
 
-        // On vérifie si isNew est true et on affiche le code
-        function verifier(datas){
-            const headers = document.querySelectorAll(".new-item");
-            const box = document.querySelectorAll('.portfolio-item')
-            for ( let i = 0 ; i < headers.length ; i++){
-
-
-            box[i].style.background = `url('${datas[i].image}')`
-            if(datas[i].isNew === "true"){
-
-                headers[i].innerHTML = `         
-                        <div class="blocNew">
-                            <h2>Nouveauté</h2>
-                        </div>
-                `;
-                headers[i].classList.add('visible')
-            } else {
-            headers[i].innerHTML = `         
-                        <div class="blocNew">
-                            <h2>Nouveauté</h2>
-                        </div>
-                `;
-                headers[i].classList.add('hidden')
-        }
-    }
-        }
-
-        // Events des boutons " Like "
-        function liked(datas){
-            const hearts = document.querySelectorAll(".heart");
-            for ( let i = 0 ; i < hearts.length ; i++){
-
-              // Récupération de la valeur isLiked sur le JSON  
-            
-                if(datas[i].isLiked === "true"){
-                    hearts[i].classList.remove('fa-regular')
-                    hearts[i].classList.add('fa-solid')
-                }else {
-                    hearts[i].classList.remove('fa-solid')
-                    hearts[i].classList.add('fa-regular')
-                }
-
-                // Au clic
-                hearts[i].addEventListener('click', () => {
-                        hearts[i].classList.toggle('fa-solid')
-
-                        if(datas[i].isLiked === "true"){
-                            hearts[i].classList.toggle('fa-regular')
-                        }
-                })
-
-            }
-        }
-
-        // Events des Logiciels, création des fonds
-        function logicielDesign(datas){
-                    
-                    for(i in datas){
-                        const parent = document.querySelectorAll ('.logiciel-items')
-                        const text = document.querySelectorAll('.text')
-
-                        for ( let i = 0 ; i < parent.length ; i++){
-
-                             if(datas[i].logiciel === "AI")
-                             {
-                                parent[i].innerHTML = `         
-                                <div class="fond">
-                                <p class="text">${datas[i].logiciel}</p>
-                                </div>
-                                `;
-                                const fond = document.querySelectorAll('.fond')
-                                fond[i].classList.add('ai')
-
-                            }
-                            else if (datas[i].logiciel === "PS")
-                            {
-                                parent[i].innerHTML = `         
-                                <div class="fond">
-                                <p class="text">${datas[i].logiciel}</p>
-                                </div>
-                                `;
-
-                                const fond = document.querySelectorAll('.fond')
-                                fond[i].classList.add('ps')
-                            } 
-                            else if(datas[i].logiciel === "Affinity Designer")
-                            {
-                                parent[i].innerHTML = `         
-                                <div class="fond">
-                                <p class="text">${datas[i].logiciel}</p>
-                                </div>
-                                `;
-
-                                const fond = document.querySelectorAll('.fond')
-                                fond[i].classList.add('afdesigner')
-
-                            }else if(datas[i].logiciel === "Affinity Publisher")
-                            {
-                                parent[i].innerHTML = `         
-                                <div class="fond afpublisher">
-                                <p class="text">${datas[i].logiciel}</p>
-                                </div>
-                                `;
-                                const fond = document.querySelectorAll('.fond')
-                                fond[i].classList.add('afpublisher')
-
-                            }
-                            else if(datas[i].logiciel === "In")
-                            {
-                                parent[i].innerHTML = `         
-                                <div class="fond">
-                                <p class="text">${datas[i].logiciel}</p>
-                                </div>
-                                `;
-
-                                const fond = document.querySelectorAll('.fond')
-                                fond[i].classList.add('in')
-                            }
-                            else
-                            {
-                                parent[i].classList.add('hidden')
-                            }
-                    }                    
-            }
-        }
-
-        function designTag(datas){
-            for(data in datas){
-                const types = document.querySelectorAll('.tag')
-                for (let i = 0; i < types.length; i++) {
-
-                    types[i].innerHTML = `
-                    <div class="tagFond">
-                        <p class="tagName">${datas[i].type}</p>
-                    </div>
-                    `
-
-                //Condition design 
-
-                if(datas[i].type == "Logo"){
-                    types[i].innerHTML = `
-                    <div class="tagFond">
-                        <p class="tagName">${datas[i].type}</p>
-                    </div>
-                    `
-                    const fond = document.querySelectorAll('.tagFond')
-                    fond[i].classList.add('logo')
-
-                } else if (datas[i].type == "Web"){
-                    types[i].innerHTML = `
-                    <div class="tagFond">
-                        <p class="tagName">${datas[i].type}</p>
-                    </div>
-                    `
-                    const fond = document.querySelectorAll('.tagFond')
-                    fond[i].classList.add('web')
-                }
-
-                }
-
-
-
-            }
-        }
-
-        verifier(datas)
-        designTag(datas)
-        liked(datas)
-        logicielDesign(datas)
+        //console.log('icone'+ramdom)
+    //    document.querySelector('#icone'+ramdom).appendChild(icone)
+    //     ramdom++ 
 
     }// Fin boucle For
 
