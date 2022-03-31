ab89f045d99c0834681bdc55776e6b155985f0c4
CarolhDesign <carole.haurat@gmail.com>
2022-03-31 12:10:27 +0200

    fix css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const parent = document.querySelector('.portfolio-header')
-let logShow
+
 const url = "article.json"
 fetch(url)
 .then(response => response.json())
@@ -24,6 +24,7 @@ function displayData(datas){
  
         // On créer une boucle for in pour parcourir les index du tableau d'objet.
         // Ensuite, on créer les variables pour une meilleure accessibilité et lisibilité du code
+
         let titre = datas[data].title
         let id = datas[data].id
         let image = datas[data].image
@@ -33,11 +34,37 @@ function displayData(datas){
         let logiciel = typeof datas[data].logiciel == "object" ? datas[data].logiciel : [datas[data].logiciel]
         let visible =  'hidden'
         const displayLiked = isLiked == 'true' ?  'fa-solid' : 'fa-regular'
-console.log(logiciel);
-      //Verifier si new Post
-            if(isNew == "true"){
-                visible = 'visible'
-            }
+
+        //Verifier si new Post
+
+        if(isNew == "true"){
+          visible = 'visible'
+        }
+
+        const displayTag = type.map(elem=>{ 
+            
+                if(elem == "Logo"){
+                    tag = 'logo'
+                } else if (elem == "Web"){
+                    tag = 'web'
+                } else if (elem == "Vectoriel"){
+                    tag = "vector"
+                } else if(elem == "Compositing"){
+                    tag = "compositing"
+                }else {
+                    tag = "hidden"
+                }  
+
+                return `
+
+                <div class="tag">
+                    <div class="tagFond ${tag}">
+                    <p class="tagName">${elem}</p>
+                </div>
+
+                `
+    
+        })  
 
         const displayLogiciel = logiciel.map(element=>{ 
             if(element == "AI"){
@@ -54,23 +81,13 @@ console.log(logiciel);
             log = "hidden"
         }    
             return `
+            <div class="logiciel-items">
                 <div class="fond ${log}">
                 <p class="text">${element}</p>
+            </div>
             `
 
         })  
-
-        //Vérifier Types
-
-        if(type == "Logo"){
-            tag = 'logo'
-        } else if (type == "Web"){
-            tag = 'web'
-        } else if (type == "Vectoriel"){
-            tag = "vector"
-        } else {
-            tag = "hidden"
-        }
 
         ul.innerHTML += `
         <li class="portfolio-item">
@@ -80,48 +97,35 @@ console.log(logiciel);
                   <div class="blocNew ${visible}">
                         <h2>Nouveauté</h2>
                   </div>
-
-                  <div class="tag">
-                  <div class="tagFond ${tag}">
-                      <p class="tagName">${type}</p>
                   </div>
+                  <div class="typeUL">
+                    ${displayTag.join("")}
                   </div>
             </div>
+
+            <div class="portfolio-content">
+                <div class="title">${titre}</div>
+            </div>
         </div>
-        <div class="portfolio-content">
-            <div class="divider"></div>
-            <div class="title">${titre}</div>
-        </div>
+        <div class="divider"></div>
+
         <div class="portfolio-footer">
         <div class="like ">
         <div class="icon"><i class="heart ${displayLiked} fa-heart"></i></div>
     </div>
         <div class="logicielUL">
-        <div class="logiciel-items">
-        ${displayLogiciel.join("")}
-        </div>
+            ${displayLogiciel.join("")}
         </div>
             </div>
         </div>
     </li>
         `;
 
-        if(logiciel >= 2){
-            logShow = "hidden";
-        } else {
-            logShow = "visible"
-        }
-
-
-
-
         //Travailler dans la boucle, chercher DATA
         const portfolioItem = document.querySelectorAll('.portfolio-item')
         portfolioItem[data].style.backgroundImage = `url('${image}')`
         portfolioItem[data].style.backgroundSize = "cover"
         
-
-
         //console.log('icone'+ramdom)
     //    document.querySelector('#icone'+ramdom).appendChild(icone)
     //     ramdom++ 
@@ -133,7 +137,5 @@ console.log(logiciel);
 
 
 /*
-    // Créer fonction Vérifier condition pour logiciel Item + 2
-    // Créer fonction condition pour type > 2 et affichage
     //Faire LocalStorage, et après Firebase
  */
